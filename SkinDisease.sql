28da6babe0f40792d0562173feb6981e8a85ff8e
depalavenkatadikshitha <122983796+depalavenkatadikshitha@users.noreply.github.com>
2023-11-27 19:37:14 +0530

    Add files via upload

---
+++ SkinDisease.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Community Edition- MySQL GUI v6.07
+Host - 5.0.67-community-nt : Database - skindisease
+*********************************************************************
+Server version : 5.0.67-community-nt
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+create database if not exists `skindisease`;
+
+USE `skindisease`;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `register` */
+
+DROP TABLE IF EXISTS `register`;
+
+CREATE TABLE `register` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) default NULL,
+  `contact` varchar(12) default NULL,
+  `email` varchar(30) default NULL,
+  `address` varchar(40) default NULL,
+  PRIMARY KEY  (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `register` */
+
+insert  into `register`(`username`,`password`,`contact`,`email`,`address`) values ('san','san','9988776655','san@gmail.com','san'),('ss','ss','9977886655','ss@gmail.com','hydbad');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
